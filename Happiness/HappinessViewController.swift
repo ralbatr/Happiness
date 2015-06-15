f6dcd76da7151a073a11e2c5179842250345b3e8
ralbatr <ralbatr@jbgsn.local>
2015-06-15 16:42:56 +0800

    第一次提交 第一堂课完成

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Ralbatr on 15/6/15.
+//  Copyright (c) 2015年 ralbatr Yi. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+
+}
